3520b271f56c8580e1e1a6c5dc3011e221026868
Jamir Gustavo <jamirgustavodev@hotmail.com>
2022-09-15 12:58:26 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio IV/Aluno.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace Exercicio_IV
+{
+    class Aluno
+    {
+        public string nome;
+        public double Primeirotrimestre;
+        public double Segundotrimestre;
+        public double Terceirotrimestre;
+        
+        public double NotaFinal()
+        {
+            return Primeirotrimestre + Segundotrimestre + Terceirotrimestre;
+        }
+
+        public bool Aprovado()
+        {
+            if (NotaFinal() >= 60.0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public double NotaRestante() {
+            if (Aprovado())
+            {
+                return 0.0;
+            }
+            else
+            {
+                return 60.0 - NotaFinal();
+            }
+        }
+    }
+}
---
+++ Exercicio IV/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicio_IV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno student = new Aluno();
+
+            Console.Write("Nome do aluno:");
+            student.nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+
+            student.Primeirotrimestre = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student.Segundotrimestre = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student.Terceirotrimestre = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("NOTA FINAL: " + student.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+
+            if (student.Aprovado())
+            {
+                Console.WriteLine(" Aprovado ");
+            }
+            else
+            {
+                Console.WriteLine(" Reprovado");
+                Console.WriteLine(" Faltam " 
+                    + student.NotaRestante().ToString("F2", CultureInfo.InvariantCulture)
+                    + " Pontos");
+            }
+        }
+    }
+}
